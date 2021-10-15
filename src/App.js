a9fefb2d4f2cd80feae7c415b2561a638a7f1c79
candacerubin <candacerubin0@gmail.com>
2021-10-15 16:42:30 -0700

    sets up folders and sass: tests sass imports; installs packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Hello World</h1>
+			<div className='card'>
+				<h1>Hello World</h1>
+			</div>
 		</div>
 	);
 }
